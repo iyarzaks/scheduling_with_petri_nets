59a9bfaa41763052130c517990115fbcfa0168fa
RotemDanino <rotemdanino1@gmail.com>
2024-12-29 17:12:20 +0200

    petri to jason- part a

---
+++ pytest.py
@@ -0,0 +1,9 @@
+from a_star_solver import init_real_problem
+
+def to_json():
+    petri_example, rcpsp_example = init_real_problem("C:/Users/User/Desktop/scheduling_with_petri_nets/extract_problems/data/j30.sm.tgz/j301_1.sm",True)
+
+
+if __name__=="__main__":
+    to_json()
+    print("HHHHHHHH")
--- rotem_ido_draft/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <Python.h>
-
-
-int main() {
-    // Print Hello, World! to the console
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
