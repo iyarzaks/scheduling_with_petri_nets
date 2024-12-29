0d56261daf88c0fc79dd4219ade391f7781d55f8
idolu <ido.lublin@gmail.com>
2024-12-29 11:34:45 +0200

    added hog2 with the help of lior

---
+++ HOG2/RCPSP/Driver.cpp
@@ -0,0 +1,12 @@
+//
+// Created by idol on 29/12/2024.
+//
+// Your First C++ Program
+
+#include <iostream>
+#include "RCPSP.h"
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
---
+++ HOG2/RCPSP/RCPSP.h
@@ -0,0 +1,17 @@
+//
+// Created by idolu on 29/12/2024.
+//
+
+#ifndef RCPSP_H
+#define RCPSP_H
+
+#include "SearchEnvironment.h"
+
+class RCPSPState{
+  };
+
+class RCPSP : public SearchEnvironment<RCPSPState,int>{
+
+  };
+
+#endif //RCPSP_H
