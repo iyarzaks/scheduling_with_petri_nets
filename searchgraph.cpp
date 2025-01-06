5577bb8b3bdae37692bfbbeed2811e79f34469c7
idolu <ido.lublin@gmail.com>
2025-01-06 23:46:58 +0200

    searchgraph 6/1/25

--- searchgraph.cpp
+++ searchgraph.cpp
@@ -1,10 +1,13 @@
 //
 // Created by idolu on 06/01/2025.
-
+#include <iostream>
+#include <vector>
 #include "searchgraph.h"
 
 std::vector<int> getAvilableTransitions(PetriExample& petri,std::map<std::string, int> marking);
 int main() {
+  int totalDuration=0;
+  int counter=1;
   PetriExample petri;
   getPetri(petri);
   RCPSP_example RCPSP;
@@ -42,25 +45,41 @@ int main() {
   std::cout<<std::endl;
 
   std::cout<<"activating transition 1"<<std::endl;
-  std::cout<<"avilable transitions"<<std::endl;
-//activate tarnsiotion only avelabe is 1
-  Transition active = petri.Transitions[0];
-  for (const auto& arc : active.arcs_out) {
-    marking[arc.first]+=arc.second;
-  }
-  for (const auto& arc : active.arcs_in) {
-    marking[arc.first]-=arc.second;
-  }
-  avilableTransitions=getAvilableTransitions(petri,marking);
+  while (1) {
+    std::cout<<"round number:"<<counter<<std::endl;
+    std::cout<<"activating transition number:"<<petri.Transitions[avilableTransitions[0]].name<<std::endl;
 
-  for (int i=0;i<avilableTransitions.size();i++) {
-    std::cout<<petri.Transitions[avilableTransitions[i]].name<<" ";
+
+    //activate tarnsiotion only avelabe is 1
+    Transition active = petri.Transitions[avilableTransitions[0]];
+    totalDuration+=active.duration;
+    for (const auto& arc : active.arcs_out) {
+      marking[arc.first]+=arc.second;
+    }
+    for (const auto& arc : active.arcs_in) {
+      marking[arc.first]-=arc.second;
+    }
+
+    std::cout<<"avilable transitions"<<std::endl;
+    avilableTransitions=getAvilableTransitions(petri,marking);
+
+    for (int i=0;i<avilableTransitions.size();i++) {
+      std::cout<<petri.Transitions[avilableTransitions[i]].name<<" ";
+    }
+    std::cout<<std::endl;
+    std::cout<<"current marking"<<std::endl;
+    for (const auto& mark : marking) {
+      if (mark.second>=1){std::cout<<mark.first<<":"<<mark.second<<" ";}
+    }
+    std::cout<<std::endl;
+    if (marking[finalstatename]>=1) {
+      std::cout<<"win"<<std::endl;
+      std::cout<<"totalduration:"<<totalDuration<<std::endl;
+      return 0;
+    }
+counter++;
   }
-  std::cout<<std::endl;
-  std::cout<<"current marking"<<std::endl;
-  for (const auto& mark : marking) {
-    if (mark.second>=1){std::cout<<mark.first<<":"<<mark.second<<" ";}
-  }
+
 }
 
 //SearchGraph search_graph;
@@ -74,7 +93,9 @@ std::vector<int> getAvilableTransitions(PetriExample& petri,std::map<std::string
       if (marking[arc.first]>=1){avilable+=arc.second;}
       requirment+=arc.second;
     }
-    if (avilable>=requirment){avilableTransitions.push_back(i);}
+    if (avilable>=requirment) {
+      avilableTransitions.insert(avilableTransitions.end(), i);
+    }
   }
   return avilableTransitions;
 }
